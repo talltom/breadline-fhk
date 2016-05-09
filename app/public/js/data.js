990da0d8212e0b347684280179bd9f4ff6b6c6dd
Tomas <tomas@uow.edu.au>
2016-05-09 23:25:10 +1000

    prototype eventbrite api interaction

---
+++ app/public/js/data.js
@@ -0,0 +1,27 @@
+//data.js - JavaScript for BreadLine FHK App EventBrite Intergration
+//requires JQuery to be loaded
+
+var eventbrite = {
+  config : {
+    token : "GSAVA4B45GPLIPWWP6D7", //Eventbrite API token *secret*
+    organizerID : "2684124262" //Eventbrite organizer ID
+  }
+};
+
+eventbrite.getEvents = function(callback){
+  $.getJSON("https://www.eventbriteapi.com/v3/events/search/?organizer.id="+eventbrite.config.organizerID+"&token="+eventbrite.config.token, function(data){
+    callback(data);
+  })
+  .fail(function(){
+    console.log("[data.getEvents] Error fetching Evenbrite event data");
+  });
+};
+
+eventbrite.getEventTickets = function(eventID, callback){
+  $.getJSON("https://www.eventbriteapi.com/v3/events/"+eventID+"/ticket_classes/?token="+eventbrite.config.token, function(data){
+    callback(data);
+  })
+  .fail(function(){
+    console.log("[data.getEventTickets] Error fetching Eventbrite event ticket data")
+  });
+};
